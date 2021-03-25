9c9f3a2abc69384a9b026cf39e3f89496cfbb9ba
DevDokus <jordy-dormans@live.nl>
2021-03-25 22:52:26 +0100

    Updated to v1.4.0
    [1]: [ Feature Added ]: With [ /coords ] you can now toggle your map coordinates.
         These will be shown around your minimap. You can change each coordinates
         position. The admin only option for this feature is not ready yet. This will
         be added in a later version.
    
    [2]: [ Feature Added (BountyHunter)]: This is one of my newly made scripts. With
         this feature you are able to hun PVE and PVP bounties. Get your bounty in
         valentine and kill your objectives. Get the evidence and claim you bounty
         at the office. This feature is fully Customizable and can even be changed
         in looks and menu position.
    [3]: Also pushed a couple of small fixes.

--- [Core]/[Vorp]/[Client]/functions.lua
+++ [Core]/[Vorp]/[Client]/functions.lua
@@ -6,10 +6,22 @@ function DrawCircle(x,y,z,r,g,b,a)
 end
 
 function Notify(text, time)
-  TriggerClientEvent("vorp:TipRight", source, text, time)
+  TriggerEvent("vorp:TipRight", text, time)
 end
 
 function DrawInfo(text, x, y, size)
+    local xc = x / 1.0;
+    local yc = y / 1.0;
+    SetScriptGfxDrawOrder(3)
+    SetTextScale(size, size)
+    SetTextCentre(true)
+    SetTextColor(255, 255, 255, 100)
+    SetTextFontForCurrentCommand(6)
+    DisplayText(CreateVarString(10, 'LITERAL_STRING', text), xc, yc)
+    SetScriptGfxDrawOrder(3)
+end
+
+function DrawCoords(text, x, y, size)
     local xc = x / 1.0;
     local yc = y / 1.0;
     SetScriptGfxDrawOrder(3)
---
+++ [Core]/[Vorp]/[Server]/BountyHunter.lua
@@ -0,0 +1,99 @@
+--------------------------------------------------------------------------------
+----------------------------------- DevDokus -----------------------------------
+--------------------------------------------------------------------------------
+local VorpCore = {}
+TriggerEvent("getCore", function(core) VorpCore = core end)
+function Wait(args) Citizen.Wait(args) end
+function Invoke(args, bool) Citizen.InvokeNative(args, bool) end
+--------------------------------------------------------------------------------
+-- Event Register
+RegisterServerEvent('DevDokus:BountyHunter:S:PayDay')
+--------------------------------------------------------------------------------
+-- Core
+--------------------------------------------------------------------------------
+-- Variables
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+AddEventHandler('DevDokus:BountyHunter:S:PayDay', function(KillCount)
+  local BPK = BountyHunter.Payment.Money.BPK
+  local XPK = BountyHunter.Payment.XP.XPK
+  local mMin = BountyHunter.Payment.Money.Min
+  local mMax = BountyHunter.Payment.Money.Max
+  local xMin = BountyHunter.Payment.XP.Min
+  local xMax = BountyHunter.Payment.XP.Max
+  local vMoney = BountyHunter.Payment.Money.Static
+  local vEXP = BountyHunter.Payment.XP.Static
+
+  local Char = VorpCore.getUser(source).getUsedCharacter
+
+  local mPay = nil
+  local xPay = nil
+
+  if BPK then
+    local PayDay = (math.floor(math.random(mMin, mMax) * KillCount))
+    Char.addCurrency(0, PayDay)
+    mPay = PayDay
+  else
+    Char.addCurrency(0, vMoney)
+    mPay = vMoney
+  end
+
+  if XPK then
+    local rEXP = (math.floor(math.random(xMin, xMax) * KillCount))
+    Char.addXp(rEXP)
+    xPay = rEXP
+  else
+    Char.addXp(vEXP)
+    xPay = vEXP
+  end
+
+  TriggerClientEvent('DevDokus:BountyHunter:C:ResetTotalKills', source)
+  TriggerClientEvent("vorp:TipRight", source, "You received $"..mPay..' and '..xPay..' XP', 5000)
+end)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------
