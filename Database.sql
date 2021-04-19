3cbc4f45300c4f9b75df4948e80480c652b9d2fc
DevDokus <49053928+DevDokus@users.noreply.github.com>
2021-04-19 02:19:49 +0200

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,8 +1,7 @@
 INSERT INTO `items` (`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES
-  -- Items for the Pick Berries Script
-	('consumable_herb_currant','Currant Herbs', 1, 1, 'item_standard', 0),
-	('blackberry', 'BlackBerry', 50, 1, 'item_standard', 0),
-  ('blueberry', 'BlueBerry', 50, 1, 'item_standard', 0),
+  ('consumable_herb_currant','Currant Herbs', 1, 1, 'item_standard', 0),
+  ('blackberry', 'BlackBerry', 50, 1, 'item_standard', 0),
+  ('blueberry', 'BlueBerry', 50, 1, 'item_standard', 0),  
   ('consumable_herb_wintergreen_berry', 'Wintergreen Berry', 50, 1, 'item_standard', 0),
   ('consumable_herb_evergreen_huckleberry', 'Everygreen Berry', 50, 1, 'item_standard', 0),
   ('consumable_orange', 'Orange', 20, 1, 'item_standard', 0),
@@ -11,6 +10,5 @@ INSERT INTO `items` (`item`, `label`, `limit`, `can_remove`, `type`, `usable`) V
   ('consumable_apple', 'Apple', 20, 1, 'item_standard', 0),
   ('oregano', 'Oregano', 25, 1, 'item_standard', 0),
   ('consumable_herb_vanilla_flower', 'Vanilla', 30, 1, 'item_standard', 0),
-  ('consumable_herb_indian_tobacco', 'Tabacco', 30, 1, 'item_standard', 0)
-	('consumable_herb_chanterelles', 'Chanterelles', 30, 1, 'item_standard', 0),
-;
+  ('consumable_herb_indian_tobacco', 'Tabacco', 30, 1, 'item_standard', 0),
+  ('consumable_herb_chanterelles', 'Chanterelles', 30, 1, 'item_standard', 0);
